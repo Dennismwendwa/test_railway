9985e496f7647145ced43c27066f81090fa514b8
Dennismwendwa <dennismusemb2@.com>
2023-04-24 06:31:47 +0300

    setting env

--- fricode/fricode/settings.py
+++ fricode/fricode/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3o2l(-k9z9t8fj1i2702!nk)obzjz-mn&^i@lomfjdnq&1@a0_' #env("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-3o2l(-k9z9t8fj1i2702!nk)obzjz-mn&^i@lomfjdnq&1@a0_' 
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,27 +88,27 @@ WSGI_APPLICATION = 'fricode.wsgi.application'
 #   }
 #}
 
-#DATABASES = {
-#	'default': {
-#		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#		'NAME': env("DB_NAME"),
-#		'USER': env("DB_USER"),
-#		'PASSWORD': env("DB_PASSWORD"),
-#		'HOST': env("DB_HOST"),
-#		'PORT': env("DB_PORT"),
-#	}
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pydev',
-        'USER': 'postgres',
-        'PASSWORD': 'dennis@5084',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': os.environ["DB_NAME"],
+		'USER': os.environ["DB_USER"],
+		'PASSWORD': os.environ["DB_PASSWORD"],
+		'HOST': os.environ["DB_HOST"],
+		'PORT': os.environ["DB_PORT"],
+	}
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'pydev',
+#        'USER': 'postgres',
+#        'PASSWORD': 'dennis@5084',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
